f5e1973b700703e962c48aa7085ad96198971779
Hesam Samimi <hesam.samimi.cs@gmail.com>
2025-03-05 04:40:36 +0000

    up

--- postgres-restore-db.sh
+++ postgres-restore-db.sh
@@ -1,3 +1,9 @@
+#! /bin/sh
+if [ "$#" -eq 0 ] || [ "$#" -eq 1 ] || [ "$#" -eq 2 ]; then
+  echo "Error: No db name and/or schema.sql file path and/or data.sql file path specified." 1>&2
+  exit 1
+fi
+
 # start in the `sql-mutations` directory
 dropdb $1 # db name
 createdb $1 # db name
